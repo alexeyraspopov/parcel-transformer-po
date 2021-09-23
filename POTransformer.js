83002cb9a09a7d2487dc750965499fe35585121d
Alexey Raspopov <oleksii.raspopov@gmail.com>
2021-09-23 10:59:08 -0400

    0.1.2

--- POTransformer.js
+++ POTransformer.js
@@ -4,9 +4,9 @@ let gtp = require('gettext-parser');
 module.exports = new Transformer({
   async transform({ asset }) {
     let code = await asset.getCode();
-    let po = gtp.po.parse(code);
+    let po = gtp.po.parse(code, 'utf-8');
     asset.type = 'js';
-    asset.setCode(`module.exports = JSON.parse(${JSON.stringify(po)});`);
+    asset.setCode(`module.exports = ${JSON.stringify(po, null, 2)};`);
     return [asset];
   },
 });
